d8ceab00a29c38535848e115ce82671bd3f24e70
Copons <copons@users.noreply.github.com>
2017-01-02 19:49:58 +0100

    NoteInfo ES6 refactor and code clean up

--- lib/note-info.jsx
+++ lib/note-info.jsx
@@ -1,157 +1,20 @@
-import React, { PropTypes } from 'react';
-import { includes, isEmpty } from 'lodash';
-import ToggleControl from './controls/toggle';
-import moment from 'moment';
-import CrossIcon from './icons/cross';
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { isEmpty } from 'lodash';
+import moment from 'moment';
+import listensToClickOutside from 'react-onclickoutside/decorator';
+
+/**
+ * Internal dependencies
+ */
 import appState from './flux/app-state';
 import { setMarkdown } from './state/settings/actions';
 import filterNotes from './utils/filter-notes';
-
-export const NoteInfo = React.createClass( {
-
-	propTypes: {
-		note: PropTypes.object,
-		markdownEnabled: PropTypes.bool,
-		onPinNote: PropTypes.func.isRequired,
-		onMarkdownNote: PropTypes.func.isRequired,
-		onOutsideClick: PropTypes.func.isRequired
-	},
-
-	mixins: [
-		require( 'react-onclickoutside' )
-	],
-
-	handleClickOutside: function() {
-		this.props.onOutsideClick();
-	},
-
-	copyPublishURL: function() {
-		this.publishUrlElement.select();
-
-		try {
-			document.execCommand( 'copy' );
-		} catch ( err ) {
-			return;
-		}
-
-		this.copyUrlElement.focus();
-	},
-
-	getPublishURL: function( url ) {
-		return isEmpty( url ) ? null : `http://simp.ly/p/${url}`;
-	},
-
-	render: function() {
-		const { isMarkdown, isPinned, note } = this.props;
-		const data = note && note.data || {};
-		const formattedDate = data.modificationDate && formatTimestamp( data.modificationDate );
-		const isPublished = includes( data.systemTags, 'published' );
-		const publishURL = this.getPublishURL( data.publishURL );
-
-		return (
-			<div className="note-info theme-color-bg theme-color-fg theme-color-border">
-				<div className="note-info-panel note-info-stats theme-color-border">
-					<div className="note-info-header">
-						<h2 className="panel-title theme-color-fg-dim">Info</h2>
-						<button type="button" className="about-done button button-borderless" onClick={this.handleClickOutside}>
-							<CrossIcon />
-						</button>
-					</div>
-					{ formattedDate &&
-						<p className="note-info-item">
-							<span className="note-info-item-text">
-								<span className="note-info-name">Modified</span>
-								<br /><span className="note-info-detail">{formattedDate}</span>
-							</span>
-						</p>
-					}
-					<p className="note-info-item">
-						<span className="note-info-item-text">
-							<span className="note-info-name">{wordCount( data && data.content )} words</span>
-						</span>
-					</p>
-					<p className="note-info-item">
-						<span className="note-info-item-text">
-							<span className="note-info-name">{characterCount( data && data.content )} characters</span>
-						</span>
-					</p>
-				</div>
-				<div className="note-info-panel note-info-pin theme-color-border">
-					<label className="note-info-item" htmlFor="note-info-pin-checkbox">
-						<span className="note-info-item-text">
-							<span className="note-info-name">Pin to top</span>
-						</span>
-						<span className="note-info-item-control">
-							<ToggleControl id="note-info-pin-checkbox" checked={isPinned} onChange={this.onPinChanged} />
-						</span>
-					</label>
-				</div>
-				<div className="note-info-panel note-info-markdown theme-color-border">
-					<label className="note-info-item" htmlFor="note-info-markdown-checkbox">
-						<span className="note-info-item-text">
-							<span className="note-info-name">Markdown</span>
-							<br /><span className="note-info-detail">
-								Enable markdown formatting on this note. <a target="_blank" href="http://simplenote.com/help/#markdown">Learn more…</a>
-							</span>
-						</span>
-						<span className="note-info-item-control">
-							<ToggleControl id="note-info-markdown-checkbox" checked={isMarkdown} onChange={this.onMarkdownChanged} />
-						</span>
-					</label>
-				</div>
-				{ isPublished &&
-					<div className="note-info-panel note-info-public-link theme-color-border">
-							<span className="note-info-item-text">
-								<span className="note-info-name">Public link</span>
-								<div className="note-info-form">
-									<input ref={e => this.publishUrlElement = e} className="note-info-detail note-info-link-text" value={publishURL} />
-									<button
-										ref={e => this.copyUrlElement = e}
-										type="button"
-										className="button button-borderless note-info-copy-button"
-										onClick={this.copyPublishURL}>Copy</button>
-								</div>
-							</span>
-					</div>
-				}
-			</div>
-		);
-	},
-
-	onPinChanged( event ) {
-		this.props.onPinNote( this.props.note, event.currentTarget.checked );
-	},
-
-	onMarkdownChanged( event ) {
-		this.props.onMarkdownNote( this.props.note, event.currentTarget.checked );
-	}
-} );
-
-function formatTimestamp( unixTime ) {
-	return moment.unix( unixTime ).format( 'MMM D, YYYY h:mm a' );
-}
-
-// https://github.com/RadLikeWhoa/Countable
-function wordCount( content ) {
-	const matches = ( content || '' )
-		.replace( /[\u200B]+/, '' )
-		.trim()
-		.replace( /['";:,.?¿\-!¡]+/g, '' )
-		.match( /\S+/g );
-
-	return ( matches || [] ).length;
-}
-
-// https://mathiasbynens.be/notes/javascript-unicode
-const surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
-function characterCount( content ) {
-	return ( content || '' )
-		// replace every surrogate pair with a BMP symbol
-		.replace( surrogatePairs, '_' )
-		// then get the length
-		.length;
-}
+import CrossIcon from './icons/cross';
+import ToggleControl from './controls/toggle';
 
 const {
 	markdownNote,
@@ -159,14 +22,204 @@ const {
 	toggleNoteInfo,
 } = appState.actionCreators;
 
+export class NoteInfo extends Component {
+
+	static propTypes = {
+		isMarkdown: PropTypes.bool,
+		isPinned: PropTypes.bool,
+		isPublished: PropTypes.bool,
+		note: PropTypes.object.isRequired,
+		onMarkdownNote: PropTypes.func.isRequired,
+		onOutsideClick: PropTypes.func.isRequired,
+		onPinNote: PropTypes.func.isRequired,
+	};
+
+	handleClickOutside = () => {
+		this.props.onOutsideClick();
+	}
+
+	onMarkdownChanged = event => {
+		const { note, onMarkdownNote } = this.props;
+		onMarkdownNote( note, event.currentTarget.checked );
+	}
+
+	onPinChanged = event => {
+		const { note, onPinNote } = this.props;
+		onPinNote( note, event.currentTarget.checked );
+	}
+
+	copyPublishUrl() {
+		this.publishUrlElement.select();
+		try {
+			document.execCommand( 'copy' );
+		} catch ( err ) {
+			return;
+		}
+		this.copyUrlElement.focus();
+	}
+
+	getPublishUrl( url ) {
+		return isEmpty( url ) ? null : `http://simp.ly/p/${ url }`;
+	}
+
+	// https://github.com/RadLikeWhoa/Countable
+	contentWordCount() {
+		const matches = this.props.note.data.content
+			.replace( /[\u200B]+/, '' )
+			.trim()
+			.replace( /['";:,.?¿\-!¡]+/g, '' )
+			.match( /\S+/g );
+
+		return ( matches || [] ).length;
+	}
+
+	// https://mathiasbynens.be/notes/javascript-unicode
+	contentCharacterCount() {
+		const surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+		return this.props.note.data.content
+			// replace every surrogate pair with a BMP symbol
+			.replace( surrogatePairs, '_' )
+			// then get the length
+			.length;
+	}
+
+	render() {
+		const {
+			note: {
+				data: {
+					modificationDate,
+					publishUrl: url,
+				},
+			},
+			isMarkdown,
+			isPinned,
+			isPublished,
+		} = this.props;
+		const formattedDate = moment.unix( modificationDate ).format( 'MMM D, YYYY h:mm a' );
+		const publishUrl = this.getPublishUrl( url );
+
+		return (
+			<div className="note-info theme-color-bg theme-color-fg theme-color-border">
+				<div className="note-info-panel note-info-stats theme-color-border">
+					<div className="note-info-header">
+						<h2 className="panel-title theme-color-fg-dim">
+							Info
+						</h2>
+						<button
+							className="about-done button button-borderless"
+							onClick={ this.handleClickOutside }
+							type="button"
+						>
+							<CrossIcon />
+						</button>
+					</div>
+					{ formattedDate &&
+						<p className="note-info-item">
+							<span className="note-info-item-text">
+								<span className="note-info-name">
+									Modified
+								</span>
+								<br />
+								<span className="note-info-detail">
+									{ formattedDate }
+								</span>
+							</span>
+						</p>
+					}
+					<p className="note-info-item">
+						<span className="note-info-item-text">
+							<span className="note-info-name">
+								{ this.contentWordCount() } words
+							</span>
+						</span>
+					</p>
+					<p className="note-info-item">
+						<span className="note-info-item-text">
+							<span className="note-info-name">
+								{ this.contentCharacterCount() } characters
+							</span>
+						</span>
+					</p>
+				</div>
+				<div className="note-info-panel note-info-pin theme-color-border">
+					<label className="note-info-item" htmlFor="note-info-pin-checkbox">
+						<span className="note-info-item-text">
+							<span className="note-info-name">
+								Pin to top
+							</span>
+						</span>
+						<span className="note-info-item-control">
+							<ToggleControl
+								checked={ isPinned }
+								id="note-info-pin-checkbox"
+								onChange={ this.onPinChanged }
+							/>
+						</span>
+					</label>
+				</div>
+				<div className="note-info-panel note-info-markdown theme-color-border">
+					<label className="note-info-item" htmlFor="note-info-markdown-checkbox">
+						<span className="note-info-item-text">
+							<span className="note-info-name">
+								Markdown
+							</span>
+							<br />
+							<span className="note-info-detail">
+								Enable markdown formatting on this note.
+								<a href="http://simplenote.com/help/#markdown" target="_blank">
+									Learn more…
+								</a>
+							</span>
+						</span>
+						<span className="note-info-item-control">
+							<ToggleControl
+								checked={ isMarkdown }
+								id="note-info-markdown-checkbox"
+								onChange={ this.onMarkdownChanged }
+							/>
+						</span>
+					</label>
+				</div>
+				{ isPublished &&
+					<div className="note-info-panel note-info-public-link theme-color-border">
+						<span className="note-info-item-text">
+							<span className="note-info-name">
+								Public link
+							</span>
+							<div className="note-info-form">
+								<input
+									className="note-info-detail note-info-link-text"
+									ref={ e => this.publishUrlElement = e }
+									value={ publishUrl }
+								/>
+								<button
+									className="button button-borderless note-info-copy-button"
+									onClick={ this.copyPublishUrl }
+									ref={ e => this.copyUrlElement = e }
+									type="button"
+								>
+									Copy
+								</button>
+							</div>
+						</span>
+					</div>
+				}
+			</div>
+		);
+	}
+
+}
+
 const mapStateToProps = ( { appState: state } ) => {
 	const filteredNotes = filterNotes( state );
 	const noteIndex = Math.max( state.previousIndex, 0 );
 	const note = state.note ? state.note : filteredNotes[ noteIndex ];
+	const { systemTags } = note.data;
 	return {
+		isMarkdown: systemTags.includes( 'markdown' ),
+		isPinned: systemTags.includes( 'pinned' ),
+		isPublished: systemTags.includes( 'published' ),
 		note,
-		isMarkdown: note.data.systemTags.includes( 'markdown' ),
-		isPinned: note.data.systemTags.includes( 'pinned' ),
 	};
 };
 
@@ -180,4 +233,6 @@ const mapDispatchToProps = ( dispatch, { noteBucket } ) => ( {
 	onPinNote: ( note, pin ) => dispatch( pinNote( { noteBucket, note, pin } ) ),
 } );
 
-export default connect( mapStateToProps, mapDispatchToProps )( NoteInfo );
+export default connect( mapStateToProps, mapDispatchToProps )(
+	listensToClickOutside( NoteInfo )
+);
